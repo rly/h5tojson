78f30f9bd4a8a401fe280f79aabf82b7169ff40b
rly <rly@lbl.gov>
2024-03-05 08:07:03 -0800

    Add pre-commit

--- h5tojson/__init__.py
+++ h5tojson/__init__.py
@@ -1,7 +1,23 @@
 from ._version import __version__
+from .conversions import H5ToJsonOpts, h5_to_dict, h5_to_json_file, h5_to_object
 from .h5tojson import H5ToJson
-from .conversions import h5_to_object, h5_to_dict, h5_to_json_file, H5ToJsonOpts
 from .models import (
+    H5ToJsonDataset,
+    H5ToJsonDatasetRefs,
+    H5ToJsonExternalLink,
+    H5ToJsonFile,
+    H5ToJsonGroup,
+    H5ToJsonSoftLink,
+    H5ToJsonTranslationOptions,
+)
+
+__all__ = [
+    __version__,
+    H5ToJson,
+    h5_to_object,
+    h5_to_dict,
+    h5_to_json_file,
+    H5ToJsonOpts,
     H5ToJsonFile,
     H5ToJsonGroup,
     H5ToJsonDataset,
@@ -9,4 +25,4 @@ from .models import (
     H5ToJsonExternalLink,
     H5ToJsonTranslationOptions,
     H5ToJsonDatasetRefs,
-)
+]
